5ce70e801ba1fde4dc2401484be3c27da2dea942
ryanalves <ryan.alves-queyssac@eleve.ensai.fr>
2026-04-07 13:17:50 +0000

    Début parse

--- src/Model/athlete.py
+++ src/Model/athlete.py
@@ -1,20 +1,4 @@
-<<<<<<< HEAD
-from typing import Union
-from src.Common.utils import parse_boolean
-
-
-class Player:
-    def __init__(self, id: int, full_name: str, is_the_goat: Union[str, bool]):
-        self.id = id
-        self.full_name = full_name
-        self.is_the_goat = parse_boolean(is_the_goat)
-
-    def __repr__(self):
-        display_string = self.full_name
-        if self.is_the_goat:
-            display_string += " (GOAT)"
-        return display_string
-=======
+"Implémentation de la classe athlète"
 from personne import Personne
 from time import datetime
 
@@ -44,4 +28,3 @@ class Athlete(Personne):
 
     def __str__(self) -> str:
         return f"Athlète : {self.nom} ({self.pays}) - ID: {self.id_personne}"
->>>>>>> 1089b7bd745a09ef037d1b52c63f25eae229d4f1
